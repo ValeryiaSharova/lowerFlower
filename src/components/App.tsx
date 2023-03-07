a1bcfb06ff7f1e0fd8f9644fcfa533ac76d6e2bd
Aller <gominzyplay@gmail.com>
2023-03-07 16:29:46 +0300

    feat: add basic configuration

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const App: React.FC = () => (
+  <div className="app" data-testid="page">
+    flower-lover
+  </div>
+);
---
+++ src/components/providers/StoryBookProvider.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react/types-6-0';
+
+// import { Provider } from 'src/components/providers/Provider';
+
+export const decorators: Required<Meta['decorators']> = [
+  (Story) => (
+    // <Provider>
+    <Story />
+    // </Provider>
+  ),
+];
