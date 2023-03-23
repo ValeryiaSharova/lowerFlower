dc2cd72ab493dd207ff105e2b114a8098ff107a2
Aller <gominzyplay@gmail.com>
2023-03-24 00:07:41 +0300

    feat: add global styles and Provider component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { Provider } from 'src/components/providers/Provider';
+
 export const App: React.FC = () => (
-  <div className="app" data-testid="page">
-    flower-lover
-  </div>
+  <Provider>
+    <div data-testid="page">flower-lover</div>
+  </Provider>
 );
