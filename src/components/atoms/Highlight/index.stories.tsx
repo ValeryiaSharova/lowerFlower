3ee0cb7f06156bd7bf75daf8aace38cf976eede8
Aller <gominzyplay@gmail.com>
2023-04-05 00:03:29 +0300

    feat: add Highlight component

---
+++ src/components/atoms/Highlight/index.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import {
+  Highlight as HighlightComponent,
+  Props,
+} from 'src/components/atoms/Highlight';
+import { decorators } from 'src/components/providers/StoryBookProvider';
+
+export default {
+  title: 'Atoms',
+  component: HighlightComponent,
+  decorators,
+} as Meta;
+
+export const Highlight: Story<Props> = (args) => (
+  <HighlightComponent {...args} />
+);
+
+Highlight.args = {
+  blur: 125,
+  color: 'pink',
+  height: 211,
+  radius: 45,
+  rotate: -43,
+  width: 472,
+};
