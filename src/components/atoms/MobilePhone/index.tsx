42795305b614018738b611a1896635b85bcb1284
Aller <gominzyplay@gmail.com>
2023-04-05 12:06:28 +0300

    feat: add size to MobilePhone component

--- src/components/atoms/MobilePhone/index.tsx
+++ src/components/atoms/MobilePhone/index.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React from 'react';
 
 import { Icon } from 'src/components/atoms/Icon';
@@ -7,11 +8,17 @@ import styles from 'src/components/atoms/MobilePhone/styles.module.css';
 export type Props = {
   isIcon?: boolean;
   phone: string;
+  size: 14 | 16;
 };
 
-export const MobilePhone: React.FC<Props> = ({ isIcon, phone }) => (
+const sizeStyles: Record<Props['size'], string> = {
+  '14': styles.size14,
+  '16': styles.size16,
+};
+
+export const MobilePhone: React.FC<Props> = ({ isIcon, phone, size }) => (
   <div className={styles.wrapper}>
     {isIcon && <Icon icon="phone" color="var(--color-green)" size={18} />}
-    <span className={styles.phone}>{phone}</span>
+    <span className={classNames(styles.phone, sizeStyles[size])}>{phone}</span>
   </div>
 );
